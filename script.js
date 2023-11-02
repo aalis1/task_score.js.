ea1ad4b17e29e714e4d3f85e779149a08dc87804
aalis1 <aalisva1@gmail.com>
2023-11-02 13:13:56 +0000

    change name writjng

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-let introScore = 200
-console.log(`This is a score for intro ${introScore}`)
+let introScore = 200;
+console.log(`This is a score for intro ${introScore}`);
 
-let gitScore = 180
-console.log(`This is a score for git ${gitScore}`)
+let gitScore = 180;
+console.log(`This is a score for git ${gitScore}`);
 
-let jsScore = 230
-console.log(`This is a score for js ${jsScore}`)
+let jsScore = 230;
+console.log(`This is a score for js ${jsScore}`);
 
-let total = introScore + gitScore + jsScore
-let averageScore = total / 3
-console.log(`This is an averageScore ${averageScore}`)
+let total = introScore + gitScore + jsScore;
+let averageScore = total / 3;
+console.log(`This is an averageScore ${averageScore}`);
 
-let studentName = "Max"
-let studentSurname = "Maksimov"
-let studentFullName = studentName + " " + studentSurname
-console.log(`Student ${studentFullName} have an averageScore ${averageScore}.`)
+let studentName = "Max";
+let studentSurname = "Maksimov";
+let studentFullName = studentName + " " + studentSurname;
+console.log(`Student ${studentFullName} have an averageScore ${averageScore}.`);
 
-let firstLetterName = studentName.slice(0,1)
-let student1FullName = firstLetterName + "." + studentSurname
-console.log(`Student ${student1FullName} have an averageScore ${averageScore}.`)
+let firstLetterName = studentName.slice(0, 1);
+let student1FullName = firstLetterName + " " + studentSurname;
+console.log(
+  `Student ${student1FullName} have an averageScore ${averageScore}.`
+);
